37e356e3e42a99106f8353fb53d4ec93b1aa73be
hashiqi <lh1357ke@163.com>
2016-12-02 12:25:20 +0800

    third-dd

--- app/src/main/java/com/example/administrator/testyy/MainActivity.java
+++ app/src/main/java/com/example/administrator/testyy/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         System.out.print("dddddddddddddddd");
         System.out.print("sdssdss");
-        System.out.print("sdsdsffffffffffffffffffffffff");
+        System.out.print("sdsdsfffffffffffffffffffffffff");
     }
 }
